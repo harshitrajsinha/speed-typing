5fdac8e768ebaca59afca0028fc851131df8b0da
Harshit <raj.harshitsinha08@gmail.com>
2025-02-22 01:14:47 +0530

    testing google fonts cache

--- service-worker.js
+++ service-worker.js
@@ -49,6 +49,10 @@ const urlsToCache = [
   "./public/fingers/right-fingers/comma.png",
   "./public/fingers/right-fingers/period.png",
   "./public/fingers/right-fingers/qmark.png",
+  "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
+  "https://fonts.googleapis.com/css2?family=Roboto+Mono:ital@0;1&display=swap",
+  "https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@1,700&display=swap",
+  "https://fonts.googleapis.com/css2?family=Libre+Bodoni:ital,wght@0,700;1,700&display=swap",
 ];
 
 self.addEventListener("install", (event) => {
